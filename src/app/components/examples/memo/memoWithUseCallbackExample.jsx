2adf76227fe231f2599ca707bbd2fd703ab32b1a
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2023-02-26 19:10:10 +0200

    Learned about React.memo. This optimization method dont rerender our component every time, if this component was rendered one time already.

--- src/app/components/examples/memo/memoWithUseCallbackExample.jsx
+++ src/app/components/examples/memo/memoWithUseCallbackExample.jsx
@@ -1,5 +1,46 @@
+import PropTypes from "prop-types";
+import React, {useCallback, useEffect, useState} from "react";
+const LogOutButton = ({onLogout}) => {
+    useEffect(() => {
+        console.log('render button')
+    })
+
+    return(
+        <button className='btn btn-primary' onClick={onLogout}>
+            Logout
+        </button>
+    )
+}
+
+LogOutButton.propTypes = {
+    onLogout: PropTypes.func
+}
+
+function areEqual (prevState, nextState) {
+    if (prevState.onLogout !== nextState.onLogout)
+        return false
+
+    return true
+}
+
+const MemoizedLogoutButton = React.memo(LogOutButton, areEqual)
+
 const MemoWithUseCallbackExample = (props) => {
-    return null;
+    const [state, setState] = useState(false)
+
+    const handleLogout = useCallback(() => {
+        localStorage.removeItem('auth')
+        console.log('logout')
+    }, [props])
+
+    return(
+        <>
+            <button className='btn btn-primary m-2' onClick={() => setState(!state)}>
+                Initiate rerender
+            </button>
+            <MemoizedLogoutButton onLogout={handleLogout} />
+        </>
+    )
 };
 
 export default MemoWithUseCallbackExample;
