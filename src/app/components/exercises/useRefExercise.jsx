5ba5f15b552c4d13559521807f205c9460f93daa
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2023-02-27 02:21:30 +0200

    Done with first task for useRef.

--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, {useRef} from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
+    const blockRef = useRef(null)
+
+    const onBlockChange = () => {
+        blockRef.current.style = 'height: 80px; width: 150px; color: white;'
+        blockRef.current.innerText = 'text'
+    }
+
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -13,6 +20,7 @@ const UseRefExercise = () => {
             </ul>
             <div
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
+                ref={blockRef}
                 style={{
                     height: 40,
                     width: 60,
@@ -21,6 +29,7 @@ const UseRefExercise = () => {
             >
                 <small>Блок</small>
             </div>
+            <button onClick={onBlockChange} className='btn btn-secondary mt-3'>change block</button>
         </CollapseWrapper>
     );
 };
